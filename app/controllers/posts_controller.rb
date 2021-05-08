deb7876c96166f2859a278a015d56fe1c1e9c2fd
phher802 <pher024@gmail.com>
2021-05-07 22:05:32 -0500

    Add new.html file

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,11 +3,28 @@ class PostsController < ApplicationController
 	def index
 	end
 
-	#
+	#call post method and call a new post
 	def new
+		@post = Post.new
 	end
 
 	def create
+		@post = Post.new(post_params)
+
+		if @post.save
+			redirect_to @post
+		else
+			render 'new'
+		end
+		
 	end
+
+	private
+	#requiring params/a secruity feature of rails
+	#if not initalized here, you can't pass data to the server
+	def post_params
+		params.require(:post).permit(:title, :content)
+	end
+
 
 end
