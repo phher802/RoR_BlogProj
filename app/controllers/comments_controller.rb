7e3aa4007a06dd5bb40cad96dd8af9f1475ee1fe
phher802 <pher024@gmail.com>
2021-05-10 23:54:22 -0500

    Add migration for comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,11 @@
 class CommentsController < ApplicationController
+	#need to have a relationship between post and comment models
+
+	def create
+		@post = Post.find(params[:post_id])
+	end
+
+	def destroy
+	end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_11_044431) do
+ActiveRecord::Schema.define(version: 2021_05_11_045018) do
 
   create_table "comments", force: :cascade do |t|
     t.string "name"
     t.text "comment"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "post_id"
   end
 
   create_table "posts", force: :cascade do |t|
