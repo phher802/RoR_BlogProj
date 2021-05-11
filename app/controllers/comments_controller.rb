b7f5b1ebfaac32bd639ee1bc63f07ec57f7c3294
phher802 <pher024@gmail.com>
2021-05-10 23:57:36 -0500

    Add create function to Comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,6 +3,8 @@ class CommentsController < ApplicationController
 
 	def create
 		@post = Post.find(params[:post_id])
+		@comment = @post.comments.create(params[:comment]).permit(:name :comment))
+		redirect_to post.path(@post)
 	end
 
 	def destroy
